608fc80bcfb98388694120db8c972cbbdb529eb1
Yakupyucee <101170223+Yakupyucee@users.noreply.github.com>
2024-11-21 15:27:31 +0300

    add findall test

--- src/main/java/tr/edu/ogu/ceng/payment/repository/SettingRepository.java
+++ src/main/java/tr/edu/ogu/ceng/payment/repository/SettingRepository.java
@@ -1,10 +1,16 @@
 package tr.edu.ogu.ceng.payment.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import tr.edu.ogu.ceng.payment.entity.Setting;
 
 @Repository
 public interface SettingRepository extends JpaRepository<Setting, Long> {
     Setting findBySettingKey(String settingKey);
+    @Query("SELECT s FROM Setting s")
+    List<Setting> findall();
 }
--- src/test/java/tr/edu/ogu/ceng/payment/repository/TestUserRepositoryTest.java
+++ src/test/java/tr/edu/ogu/ceng/payment/repository/TestUserRepositoryTest.java
@@ -1,15 +1,17 @@
 package tr.edu.ogu.ceng.payment.repository;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import tr.edu.ogu.ceng.payment.entity.Setting;
-import tr.edu.ogu.ceng.payment.repository.SettingRepository;
 
 @SpringBootTest
 public class TestUserRepositoryTest {
@@ -46,6 +48,27 @@ public class TestUserRepositoryTest {
         assertThat(foundSetting).isNotNull();
         assertThat(foundSetting.getSettingKey()).isEqualTo("testKey");
         assertThat(foundSetting.getSettingValue()).isEqualTo("testValue");
+    }
+    @Query
+    @Test
+    public void testFindAll() {
+        // Arrange
+        Setting setting1 = new Setting();
+        setting1.setSettingKey("key1");
+        setting1.setSettingValue("value1");
+        settingRepository.save(setting1);
+
+        Setting setting2 = new Setting();
+        setting2.setSettingKey("key2");
+        setting2.setSettingValue("value2");
+        settingRepository.save(setting2);
+
+        // Act
+        List<Setting> settings = settingRepository.findAll();
+
+        // Assert
+        assertThat(settings).isNotEmpty();
+        assertThat(settings.size()).isGreaterThanOrEqualTo(2);
     }
 
     @DynamicPropertySource
